54bf19ab207cac08120814638f807139469ac543
Adella <adellakenn@gmail.com>
2019-04-25 14:16:29 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -4,6 +4,7 @@ def levitation_quiz
   	puts "What is the spell that enacts levitation?"  # "What is the spell that enacts levitation?"
 	  answer = gets # gets answer
 	  break if answer == "Wingardium Leviosa" # HAS to equal "Wingardium Leviosa"
+  end	 
 	  puts "You passed the quiz!"  # only if answere correctly
   end
 end
